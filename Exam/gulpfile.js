c8b100478721ca470ccf07152b7af9a0491cb066
PaladinZP <alexandr.taa@gmail.com>
2018-06-24 15:57:05 +0300

    Exam almost done

---
+++ Exam/gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp'),
+    minify = require('gulp-clean-css'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    // uglify = require('gulp-uglify'),
+    watch = require('gulp-watch'),
+    imagemin = require('gulp-imagemin'),
+    bs = require('browser-sync').create();
+
+gulp.task('default', ['browsersync', 'watch']);
+
+gulp.task('browsersync', function () {
+    bs.init({
+        server: "./",
+        port: 8080
+    });
+});
+
+gulp.task('watch', function() {
+    gulp.watch('scss/**/*.scss', ['style']);
+    // gulp.watch('src/js/script.js', ['script']);
+    bs.watch('*.html').on('change', bs.reload);
+});
+
+gulp.task('style', function() {
+    return gulp.src('scss/style.scss', {style : 'expended'})
+        .pipe(plumber())
+        .pipe(sass({includePaths: ['sass/**']}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minify())
+        .pipe(gulp.dest('css/'))
+        .pipe(notify({message: 'Style task is finished'}))
+        .pipe(bs.reload({stream: true}));
+});
+
+gulp.task('script', function() {
+    return gulp.src('src/js/script.js')
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('js/'))
+        .pipe(notify({message: 'Script task is finished'}))
+        .pipe(bs.reload({stream: true}));
+});
+
+gulp.task('imagemin', function() {
+    return gulp.src('imgsrc/**')
+        .pipe(imagemin())
+        .pipe(gulp.dest('img/'));
+});
